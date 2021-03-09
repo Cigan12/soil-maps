933204cebb8d3a4f1bda18d20d45107040b3a265
Cigan <darknew1998@mail.ru>
2021-03-09 12:35:33 +0700

    map completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useState } from 'react';
 import './App.scss';
+import { Map } from './components/Map/Map.component';
+import { URectangle } from './utils/Rectangles.util';
+import Leaflet, { Map as LeafletMap } from 'leaflet';
 
 export const App: React.FC = () => {
+    const [map, setMap] = useState<null | LeafletMap>(null);
     return (
         <div className="App">
-            <MapContainer
-                center={[51.505, -0.09]}
-                zoom={13}
-                scrollWheelZoom={false}
+            <Map onCreated={setMap} />
+            <div
+                className="Layers"
+                onClick={() =>
+                    map?.panTo([
+                        (URectangle.coords[1][0] + URectangle.coords[0][0]) / 2,
+                        (URectangle.coords[1][1] + URectangle.coords[0][1]) / 2,
+                    ])
+                }
             >
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
+                <h5>{URectangle.title}</h5>
+                <p>
+                    {URectangle.coords[0][0]} {URectangle.coords[0][1]}
+                </p>
+            </div>
         </div>
     );
 };
---
+++ src/components/Map/Map.component.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import 'leaflet/dist/leaflet.css';
+import Leaflet, { Map as LeafletMap } from 'leaflet';
+import {
+    MapContainer,
+    Marker,
+    Popup,
+    Rectangle,
+    TileLayer,
+} from 'react-leaflet';
+import './Map.styles.scss';
+import { URectangle } from '../../utils/Rectangles.util';
+
+interface IMapProps {
+    onCreated: (map: LeafletMap) => void;
+}
+
+export const Map: React.FC<IMapProps> = ({ onCreated }) => {
+    const MarkerIcon = Leaflet.icon({
+        iconUrl: '/logo192.png',
+        className: 'Icon',
+    });
+
+    return (
+        <MapContainer
+            center={[55.034242, 82.928757]}
+            zoom={19}
+            className="Map"
+            whenCreated={onCreated}
+        >
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[55.034242, 82.928757]} icon={MarkerIcon}>
+                <Popup>Маркер на нашем институте</Popup>
+            </Marker>
+            <Rectangle
+                bounds={URectangle.coords}
+                pathOptions={{ color: '#FF0000' }}
+                eventHandlers={{
+                    click: () => console.log('click on layer'),
+                    mouseover: () => console.log('hover'),
+                }}
+            >
+                <Popup>
+                    {URectangle.title}
+                    <br />
+                    {(
+                        (URectangle.coords[1][0] + URectangle.coords[0][0]) /
+                        2
+                    ).toFixed(4)}{' '}
+                    {(
+                        (URectangle.coords[1][1] + URectangle.coords[0][1]) /
+                        2
+                    ).toFixed(4)}
+                </Popup>
+            </Rectangle>
+        </MapContainer>
+    );
+};
